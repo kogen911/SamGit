449478f73a2865d28051328253f4b0f26d24018f
kogen <19cm0110@jec.ac.jp>
2020-12-01 10:48:30 +0900

    Initial Commit

---
+++ SamGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SamGit
+//
+//  Created by Kogen's Air on 2020/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
